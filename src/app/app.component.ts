0dd60c3b112ce693331165dae710a49c600766af
Vincent Tabary <1731605+vtabary@users.noreply.github.com>
2023-01-14 15:52:08 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'visual-show-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'visual-show';
+}
